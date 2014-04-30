53eeda16b8cefa2ada7fcba1ab6fa794eedfb579
mikenewt <mikejnewt@gmail.com>
2014-04-30 13:52:01 +0800

    Implemented enter key functionality.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
 	$('#add-items').keydown(function(event) {
 		if (event.which == 13) {
 			var item = $(this).val();
-			$('<div>' + item + '</div>').prependTo('.shopping-list').addClass('need');
+			$('<div><p id="item">' + item + '</p></div>').prependTo('.shopping-list').addClass('need');
 			$('#add-items').val('');
 		}
 	});
+	$()
 })
